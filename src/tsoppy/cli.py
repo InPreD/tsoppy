06ee50d0dae1fbc7b4a580c1d03703d25a48754e
tina <15641075+tinavisnovska@users.noreply.github.com>
2026-03-26 10:21:33 +0100

    feat: add report_predispositions function into src/tsoppy/cli.py

--- src/tsoppy/cli.py
+++ src/tsoppy/cli.py
@@ -33,3 +33,27 @@ def placeholder(
         print(f"{user_name} has the following id: {user_id}")
     else:
         print(f"{user_name}: {user_id}")
+
+@app.command()
+def report_predispositions(
+        cancer_susceptibility_genes: Annotated[str, typer.Option("--cancer-susceptibility-genes", "-g")],
+        small_variant_calls: Annotated[str, typer.Option("--small-variant-calls", "-c")],
+        age: Annotated[int, typer.Option("--age", "-a")],
+        output: Annotated[str, typer.Option("--output", "-o") ],
+        verbose: Annotated[bool, typer.Option("--verbose", "-v")] = False,
+):
+    """
+    This function reports variants called by small variant caller that are present 
+    in the cancer susceptibility genes.    
+    """
+    
+    predispositions = dict()
+
+    if verbose:
+        print(f"Load data from the {cancer_susceptibility_genes} table")
+    if verbose:
+        print(f"Open the {small_variant_calls} file and iterate through the variants. 
+                Store all the variants present in the {cancer_susceptibility_genes} table 
+                together with all the info that should be reported into the {predispositions}.")
+    if verbose:
+        print(f"Print the {predispositions} content into the {output} file.")
