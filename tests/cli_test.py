f5785d2088d133681c06ad4f86255399342e32e7
tina <15641075+tinavisnovska@users.noreply.github.com>
2026-04-02 15:35:52 +0200

    test: add tests for report_predispositions subpackage

--- tests/cli_test.py
+++ tests/cli_test.py
@@ -8,3 +8,11 @@ def test_placeholder():
     Unit test for the placeholder command in the CLI module.
     """
     assert True
+
+
+# TODO
+def test_report_predispositions():
+    """
+    Unit test for the report predisposition command in the CLI module.
+    """
+    assert True
---
+++ tests/report_predispositions/report_predispositions_test.py
@@ -0,0 +1,12 @@
+"""
+Report predispositions subpackage unit test.
+"""
+
+# TODO
+
+
+def test_report_predispositions():
+    """
+    Unit test for the report predisposition function of the subpackage.
+    """
+    assert True
