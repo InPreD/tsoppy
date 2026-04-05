e76b6587a2e95dbd20bda135d77fbb3442dcb42c
tina <15641075+tinavisnovska@users.noreply.github.com>
2026-04-05 22:12:13 +0200

    test: add test for the load_data_from_cancer_susceptibility_genes_table function

--- tests/reportPredispositions/predispositions_test.py
+++ tests/reportPredispositions/predispositions_test.py
@@ -2,6 +2,33 @@
 Report predispositions subpackage unit test.
 """
 
+import os
+
+from tsoppy.reportPredispositions.predispositions import load_data_from_cancer_susceptibility_genes_table, report_predispositions
+
+
+def test_load_data_from_cancer_susceptibility_genes_table(tmp_path):
+
+    # create temporary file
+    directory = tmp_path / 'tsoppy_test'
+    directory.mkdir()
+    test_file = directory / 'csg.csv'
+
+    # write content to the temp file
+    csg_table_content = "Gene\tActionability\tAge\nTP53\tHA-CSG\tAge<30\n"
+    test_file.write_text(csg_table_content)
+
+    # create result
+    result = dict()
+    result['TP53'] = dict()
+    result['TP53']['Actionability'] = 'HA-CSG'
+    result['TP53']['Age'] = 'Age<30'
+
+    # pass the file to the function
+    assert load_data_from_cancer_susceptibility_genes_table(
+        test_file) == result
+
+
 # TODO
 
 
