8f69311c5d4bc54fcb5f0131e0d02197885b4dca
Martin Rippin <marrip@ihelse.net>
2026-06-05 15:46:46 +0200

    test: add test_data and unittests for Parse_section_Tsv

---
+++ tests/general_file_parser_test.py
@@ -0,0 +1,167 @@
+import polars
+from os import path
+from pytest import mark
+
+
+from tsoppy.general.file_parser import Parse_section_tsv
+
+# Define path to test data - cannot be absolute due to different paths locally and in CI
+test_data_dir = "tests/test_data/general"
+
+
+@mark.parametrize(
+    "inputs, want",
+    [
+        (
+            (
+                path.join(test_data_dir, "parse_section_tsv/standard.tsv"),
+                []
+            ),
+            (
+                [
+                    "header1"
+                ],
+                {
+                    "section1": polars.DataFrame(
+                        {
+                            "col1": ["value1", "value2"],
+                            "col2": ["value3", "value4"]
+                        }
+                    )
+                }
+            )
+        ),
+        (
+            (
+                path.join(test_data_dir,
+                          "parse_section_tsv/multiple_sections.tsv"),
+                []
+            ),
+            (
+                [
+                    "header1"
+                ],
+                {
+                    "section1": polars.DataFrame(
+                        {
+                            "col1": ["value1", "value2"],
+                            "col2": ["value3", "value4"]
+                        }
+                    ),
+                    "section2": polars.DataFrame(
+                        {
+                            "col1": ["value1", "value2"],
+                            "col2": ["value3", "value4"]
+                        }
+                    )
+                }
+            )
+        ),
+        (
+            (
+                path.join(test_data_dir,
+                          "parse_section_tsv/no_headers.tsv"),
+                []
+            ),
+            (
+                [],
+                {
+                    "section1": polars.DataFrame(
+                        {
+                            "col1": ["value1", "value2"],
+                            "col2": ["value3", "value4"]
+                        }
+                    )
+                }
+            )
+        ),
+        (
+            (
+                path.join(test_data_dir,
+                          "parse_section_tsv/extra_empty_lines.tsv"),
+                []
+            ),
+            (
+                [
+                    "header1"
+                ],
+                {
+                    "section1": polars.DataFrame(
+                        {
+                            "col1": ["value1", "value2"],
+                            "col2": ["value3", "value4"]
+                        }
+                    )
+                }
+            )
+        ),
+        (
+            (
+                path.join(test_data_dir,
+                          "parse_section_tsv/null_columns.tsv"),
+                []
+            ),
+            (
+                [
+                    "header1"
+                ],
+                {
+                    "section1": polars.DataFrame(
+                        {
+                            "col1": ["value1", "value2"],
+                            "col2": ["value3", "value4"]
+                        }
+                    )
+                }
+            )
+        ),
+        (
+            (
+                path.join(test_data_dir,
+                          "parse_section_tsv/empty_first_column_name.tsv"),
+                []
+            ),
+            (
+                [
+                    "header1"
+                ],
+                {
+                    "section1": polars.DataFrame(
+                        {
+                            "-": ["value1", "value2"],
+                            "col2": ["value3", "value4"]
+                        }
+                    )
+                }
+            )
+        ),
+        (
+            (
+                path.join(test_data_dir,
+                          "parse_section_tsv/key_value.tsv"),
+                [
+                    "section1"
+                ]
+            ),
+            (
+                [
+                    "header1"
+                ],
+                {
+                    "section1": polars.DataFrame(
+                        {
+                            "key1": ["value1"],
+                            "key2": ["value2"]
+                        }
+                    )
+                }
+            )
+        ),
+    ]
+)
+def test_parse_section_tsv(inputs, want):
+    got = Parse_section_tsv(inputs[0], inputs[1])
+    assert got[0] == want[0]
+    for key in want[1].keys():
+        assert key in got[1]
+        assert got[1][key].equals(want[1][key])
