c6a8a7aa2a1c31e880cbb96693f937faa6950fbc
Martin Rippin <marrip@ihelse.net>
2026-06-05 16:05:38 +0200

    test: add test cases and data for empty file and non-existent

--- tests/general_file_parser_test.py
+++ tests/general_file_parser_test.py
@@ -1,6 +1,7 @@
 import polars
 from os import path
-from pytest import mark
+from pytest import mark, raises
+from contextlib import nullcontext
 
 
 from tsoppy.general.file_parser import Parse_section_tsv
@@ -10,13 +11,14 @@ test_data_dir = "tests/test_data/general"
 
 
 @mark.parametrize(
-    "inputs, want",
+    "inputs, exception, want",
     [
         (
             (
                 path.join(test_data_dir, "parse_section_tsv/standard.tsv"),
                 []
             ),
+            nullcontext(),
             (
                 [
                     "header1"
@@ -37,6 +39,7 @@ test_data_dir = "tests/test_data/general"
                           "parse_section_tsv/multiple_sections.tsv"),
                 []
             ),
+            nullcontext(),
             (
                 [
                     "header1"
@@ -63,6 +66,7 @@ test_data_dir = "tests/test_data/general"
                           "parse_section_tsv/no_headers.tsv"),
                 []
             ),
+            nullcontext(),
             (
                 [],
                 {
@@ -81,6 +85,7 @@ test_data_dir = "tests/test_data/general"
                           "parse_section_tsv/extra_empty_lines.tsv"),
                 []
             ),
+            nullcontext(),
             (
                 [
                     "header1"
@@ -101,6 +106,7 @@ test_data_dir = "tests/test_data/general"
                           "parse_section_tsv/null_columns.tsv"),
                 []
             ),
+            nullcontext(),
             (
                 [
                     "header1"
@@ -121,6 +127,7 @@ test_data_dir = "tests/test_data/general"
                           "parse_section_tsv/empty_first_column_name.tsv"),
                 []
             ),
+            nullcontext(),
             (
                 [
                     "header1"
@@ -143,6 +150,7 @@ test_data_dir = "tests/test_data/general"
                     "section1"
                 ]
             ),
+            nullcontext(),
             (
                 [
                     "header1"
@@ -157,11 +165,36 @@ test_data_dir = "tests/test_data/general"
                 }
             )
         ),
+        (
+            (
+                path.join(test_data_dir,
+                          "parse_section_tsv/non-existent.tsv"),
+                []
+            ),
+            raises(FileNotFoundError),
+            (
+                [],
+                {}
+            )
+        ),
+        (
+            (
+                path.join(test_data_dir,
+                          "parse_section_tsv/empty.tsv"),
+                []
+            ),
+            raises(polars.exceptions.NoDataError),
+            (
+                [],
+                {}
+            )
+        )
     ]
 )
-def test_parse_section_tsv(inputs, want):
-    got = Parse_section_tsv(inputs[0], inputs[1])
-    assert got[0] == want[0]
-    for key in want[1].keys():
-        assert key in got[1]
-        assert got[1][key].equals(want[1][key])
+def test_parse_section_tsv(inputs, exception, want):
+    with exception:
+        got = Parse_section_tsv(inputs[0], inputs[1])
+        assert got[0] == want[0]
+        for key in want[1].keys():
+            assert key in got[1]
+            assert got[1][key].equals(want[1][key])
