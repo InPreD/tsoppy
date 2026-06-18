e22425c11b376e43f8786232daafee223d5508f2
Omer Ali <omerali.0191@gmail.com>
2026-06-18 13:54:19 +0200

    updated in and example command

---
+++ src/tsoppy/metric_plots/example_command.sh
@@ -0,0 +1,4 @@
+# in the folder in/both* I have added some files from dragen and localapp to see if code can correctly process them
+# the master metrics table and joint sequencing QC file should be generated with both files.
+
+python make_master_metrics_table.py  --input-directory in/both_dragen_localapp
