1cf600561d7b75f49f62dd5ff2ff09669e1268c3
tina <15641075+tinavisnovska@users.noreply.github.com>
2026-07-15 16:42:09 +0200

    fix: correct import report_predispositions
    Co-authored-by: Martin Rippin <74295098+marrip@users.noreply.github.com>

--- src/tsoppy/cli.py
+++ src/tsoppy/cli.py
@@ -8,7 +8,7 @@ from typing import Annotated
 
 import typer
 
-from tsoppy.reportPredispositions.main import report_predispositions
+from tsoppy.report_predispositions.main import report_predispositions
 
 app = typer.Typer()
 app_version = importlib.metadata.version("tsoppy")
