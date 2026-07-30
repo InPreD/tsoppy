aa284bf22bdc67addd5a5ef607f5a89726bcd6ee
Developer <developer@example.com>
2026-07-30 14:48:49 +0200

    updated main.py to include LSL and USL values from MetricPutput.tsv, did ruff fomrat as well

--- src/tsoppy/metric_plots/example_command.sh
+++ src/tsoppy/metric_plots/example_command.sh
@@ -1,4 +1,5 @@
 # in the folder in/both* I have added some files from dragen and localapp to see if code can correctly process them
 # the master metrics table and joint sequencing QC file should be generated with both files.
 
-python make_master_metrics_table.py  --input-directory in/both_dragen_localapp
+tsoppy metric-plots --input-directory tests/test_data/metric_plots/in/both_dragen_localapp/
+--run-id-file tests/test_data/metric_plots/in/run_ids.txt --output-directory tests/test_data/metric_plots/out/ --no-create-plots
