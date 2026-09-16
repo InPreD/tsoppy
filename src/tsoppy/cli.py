ea87bb4bdb873b6a815dbdb0222b140c6b793879
Martin Rippin <marrip@ihelse.net>
2026-09-16 15:32:07 +0200

    feat: add commonOptions for subcommands

--- src/tsoppy/cli.py
+++ src/tsoppy/cli.py
@@ -3,12 +3,52 @@ This module contains the CLI commands for tsoppy.
 """
 
 import importlib.metadata
+import tempfile
+from dataclasses import dataclass
+from pathlib import Path
 from typing import Annotated
+from urllib import request
 
 import typer
 
 app = typer.Typer()
 app_version = importlib.metadata.version("tsoppy")
+
+
+@dataclass
+class CommonOptions:
+    """Holds common input options."""
+    nomenclature: Path
+    config: Path
+
+
+def nomenclature_callback(value: Path | None) -> Path:
+    """
+    Callback function to provide fallback to nomenclature.yaml available on GitHub in case of missing or non-existent file.
+    """
+    if value is None or not value.exists():
+        temp_file = tempfile.NamedTemporaryFile(delete=False)
+        # TODO: Update url when PR is merged to main branch
+        request.urlretrieve(
+            "https://raw.githubusercontent.com/InPreD/reference/refs/heads/5-add-inpred-nomenclature/InPreD/sample_id_nomenclature/v4/nomenclature.yaml", temp_file.name)
+        return Path(temp_file.name)
+    else:
+        return value
+
+
+@app.callback()
+def main(
+    ctx: typer.Context,
+    config: Annotated[Path, typer.Option(
+        "--config", help="Path to tsoppy config file.", exists=True)],
+    nomenclature: Annotated[Path | None, typer.Option(
+        "--nomenclature", help="Path to inpred nomenclature file.", callback=nomenclature_callback)] = None,
+):
+    """
+    This is the main entry point for the tsoppy CLI application. It sets up the
+    context object with commonOptions that can be accessed by subcommands.
+    """
+    ctx.obj = CommonOptions(nomenclature=nomenclature, config=config)
 
 
 @app.command()
@@ -21,15 +61,11 @@ def version():
 
 @app.command()
 def placeholder(
-    user_name: Annotated[str, typer.Option("--name", "-n")],
-    user_id: Annotated[str, typer.Option("--id", "-i")],
-    verbose: Annotated[bool, typer.Option("--verbose", "-v")] = False,
+    ctx: typer.Context,
 ):
     """
-    This is the helptext for the placeholder command that demonstrates how to
-    use Typer for CLI applications.
+    This command illustrates how to include the commonOptions in a subcommand.
     """
-    if verbose:
-        print(f"{user_name} has the following id: {user_id}")
-    else:
-        print(f"{user_name}: {user_id}")
+    commonOptions = ctx.obj
+    print(commonOptions.config)
+    print(commonOptions.nomenclature)
